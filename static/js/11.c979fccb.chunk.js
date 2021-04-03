f1027a74317461ba7e6e54f35935ab19905f199c
Justyna Olszewka <wioza@wp.pl>
2021-04-03 18:03:20 +0200

    Updates

--- static/js/11.c979fccb.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpplaylist=this.webpackJsonpplaylist||[]).push([[11],{106:function(t,n,s){"use strict";s.r(n);s(0);var a,e=s(19),i=s(17).default.h1(a||(a=Object(e.a)(["\n  position: absolute;\n  top: 50%;\n  left: 50%;\n  transform: translate(-50%, -50%);\n"]))),l=s(3);n.default=function(){return Object(l.jsx)(i,{children:"Enjoy"})}}}]);
-//# sourceMappingURL=11.c979fccb.chunk.js.map
---
+++ static/js/13.553be19e.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpplaylist=this.webpackJsonpplaylist||[]).push([[13],{100:function(t,s,i){"use strict";i.r(s);i(0);var n=i(3);s.default=function(){return Object(n.jsx)("div",{children:"SongDetails"})}}}]);
+//# sourceMappingURL=13.553be19e.chunk.js.map
